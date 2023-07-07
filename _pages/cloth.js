0e90e74991f590d9b78f4e18ce57aa0c4a7bd03f
Tharaka Jayasena <89380461+TharakaUJ@users.noreply.github.com>
2023-07-07 13:05:41 +0500

    test

--- _pages/cloth.js
+++ _pages/cloth.js
@@ -23,7 +23,7 @@ let ctx = canvas.getContext('2d')
 canvas.width = Math.min(300, window.innerWidth)
 canvas.height = 400
 
-ctx.strokeStyle = '#000'
+ctx.strokeStyle = '#fff'
 
 let mouse = {
   cut: 8,
