b53d59eabb7a7db0d577a239e5b69570c7d8e880
TharakaUJ <9dmpires2k17.tuj@gmail.com>
2023-07-30 11:16:12 +0530

    created a include for braking into pages

--- assets/js/myfunctions.js
+++ assets/js/myfunctions.js
@@ -39,3 +39,57 @@ let currents = document.getElementsByClassName("myactive");
 
 
 */
+
+
+ 
+ const main = document.getElementById('main');
+ const lis = main.getElementsByTagName('li');
+ const sections = document.getElementsByTagName("section");
+ const back = document.getElementById('back');
+ const next = document.getElementById('next');
+ 
+ 
+ 
+ [...lis].forEach(li => {
+     li.addEventListener('click', function() {
+         gotoElement([...lis].indexOf(this));
+     });
+ });
+ 
+ back.addEventListener('click', function() {
+     let currents = document.getElementsByClassName("active");
+     let index = [...sections].indexOf(currents[0]);
+     gotoElement(index-1);
+ });
+ 
+ next.addEventListener('click', function() {
+     let currents = document.getElementsByClassName("active");
+     let index = [...sections].indexOf(currents[0]);
+     gotoElement(index+1);
+ });
+ 
+ 
+ 
+ function gotoElement(i) {
+     let currents = document.getElementsByClassName("active");
+     currents[0]?.classList.remove('active');
+     sections[i].classList.add('active');
+     let activeLis = main.getElementsByClassName('myactive');
+     activeLis[0]?.classList.remove('myactive');
+     lis[i].classList.add('myactive');
+     switch (i) {
+         case 0:
+             back.style.display = 'none';
+             break;
+         case sections.length-1:
+             next.style.display = 'none';
+             break;
+         default:
+             back.style.display = 'inline';
+             next.style.display = 'inline';
+ 
+     }
+ }
+ 
+ //adding 'myactive' class to 1st item on list
+ gotoElement(0)
